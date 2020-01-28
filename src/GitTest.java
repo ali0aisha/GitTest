f8b9ae3346a303a83281e60ffeb57e90e1cdfea1
ali0aisha <u1865309@unimail.hud.ac.uk>
2020-01-28 16:41:14 +0000

    Second commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,6 +2,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Aisha is a bulla");
+        System.out.println("Aisha is a amazing");
     }
 }
